6b75b38355f705ce053a62da7bb4d9a0f10ebc5a
Hectormbz <hectorbzzctt@gmail.com>
2025-10-06 12:25:43 +0100

    calender complete

---
+++ calender.py
@@ -0,0 +1,16 @@
+import sys
+numDays = int(input("How many days in the month"))
+firstDay = int(input("What day does it start on: enter 1 for sunday, 2 for monday etc..."))
+print("S   M   T   W   T   F   S\n")
+for i in range(firstDay - 1):
+    print("-- ", end = ' ')
+for a in range(8 - firstDay):
+    print(a+1," ", end = ' ')
+b = 8 - firstDay
+while b < numDays:
+    print("\n")
+    for c in range(7):
+        if b == numDays:
+            sys.exit()
+        b = b + 1
+        print(0+b, " ", end = ' ')
