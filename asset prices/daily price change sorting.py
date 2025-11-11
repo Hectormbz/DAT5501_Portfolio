2ffe2ff1c439764dcc25dd545ad19e90dc427e5a
Hectormbz <hectorbzzctt@gmail.com>
2025-11-11 16:37:01 +0000

    daily price change sorting attempt (does not seem to follow the logarithmic pattern)

---
+++ asset prices/daily price change sorting.py
@@ -0,0 +1,48 @@
+import os
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from time import perf_counter
+# read data
+
+csv_path = r"C:\Users\hbuzzacott001\OneDrive - PwC\Uni work\YR2\DAT5501_Portfolio\asset prices\HistoricalData_1760959707604.csv"
+df = pd.read_csv(csv_path)
+
+# Ensure 'Close/Last' is float
+df['Close/Last'] = df['Close/Last'].str.replace('$','').astype(float)
+#creates list of daily price changes
+dailyChanges = df['Close/Last'].diff().dropna().to_numpy()
+
+# n values to test sorting time
+ns = np.arange(7, 366)
+# Time the sorting
+times = []
+for n in ns:
+    sample = dailyChanges[:n].astype(float)  # view of first n daily changes
+    np.sort(sample)  # Warm-up sort, not timed
+    repeats = 20  # Number of times to repeat the process for averaging
+    t0 = perf_counter()
+    for i in range(repeats):
+        np.sort(sample)
+    t = (perf_counter() - t0) / repeats
+    times.append(t)
+
+#make lists into numpy arrays
+ns = np.array(ns)
+times = np.array(times)
+
+# get n log n
+nlog = (ns * np.log2(ns))/1000000000
+
+
+# Plot results
+plt.figure(figsize=(8,5))
+plt.plot(ns, times, label='Measured sort time (s)', marker='o', markersize=4)
+plt.plot(ns, nlog, label='n·log2(n)', linestyle='--')
+plt.xlabel('n (number of ΔP elements sorted)')
+plt.ylabel('Time (seconds)')
+plt.title('Sorting time T vs n  — compare with n·log n')
+plt.legend()
+plt.grid(True)
+plt.tight_layout()
+plt.show()
--- election data/election.py
+++ election data/election.py
@@ -2,9 +2,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 #load csv file
-df = pd.read_csv("US-2016-primary.csv", sep = ';')
+df = pd.read_csv(r"C:\Users\hbuzzacott001\OneDrive - PwC\Uni work\YR2\DAT5501_Portfolio\election data\US-2016-primary.csv", sep = ';')
 
 #select candidate
 candidateName = 'Bernie Sanders'
 candidateDF = df[df['candidate'].str.contains(candidateName, na = False)]
 
+print(candidateDF)
