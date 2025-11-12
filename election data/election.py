f427f524b340f934f8b8fe78b7ec47fc5a7c2bea
Hectormbz <hectorbzzctt@gmail.com>
2025-11-12 16:20:20 +0000

    Histogram for votes

--- election data/election.py
+++ election data/election.py
@@ -8,4 +8,19 @@ df = pd.read_csv(r"C:\Users\hbuzzacott001\OneDrive - PwC\Uni work\YR2\DAT5501_Po
 candidateName = 'Bernie Sanders'
 candidateDF = df[df['candidate'].str.contains(candidateName, na = False)]
 
-print(candidateDF)
+# Group by state and candidate, sum votes
+state_totals = df.groupby("state")["votes"].sum()
+candidate_votes = candidateDF.groupby("state")["votes"].sum()
+
+# Compute fraction of votes for the candidate per state
+vote_fraction = (candidate_votes / state_totals).dropna()
+
+# Plot histogram
+plt.figure(figsize=(10, 6))
+plt.hist(vote_fraction, bins=15, edgecolor='black')
+plt.title(f"Histogram of Fraction of Votes for {candidateName} by State")
+plt.xlabel("Fraction of Votes")
+plt.ylabel("Number of States")
+plt.grid(True, alpha=0.3)
+plt.show()
+
